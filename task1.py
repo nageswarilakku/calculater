e51f9ea96d38df63ab50e4a917b6369c1a915179
nageswarilakku <152404707+nageswarilakku@users.noreply.github.com>
2024-05-01 05:24:07 -0500

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,16 @@
+#simple calculator
+num1= int(input("enter first number:"))
+op=input("enter operator:")
+num2=int(input("enter second number:"))
+if op=="+":
+    print(num1+num2)
+elif op =="-":
+    print(num1-num2)
+elif op=="*":
+    print(num1*num2)
+elif op=="/":
+    print(num1/num2)
+elif op=="%":
+    print(num1%num2)
+else:
+    print("invalid operator")
